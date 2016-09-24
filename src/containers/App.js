0fa1555daaf701ed3874d93d002450ef764d3ed7
Andrew Fung <afung95014@gmail.com>
2016-09-24 00:10:59 -0700

    added css file

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
 import Navbar from '../components/Navbar';
 import PatientList from '../components/PatientList';
+import CurrentPatientView from '../components/CurrentPatientView';
+
+
+const testPatient = {
+  patientId: 123,
+  firstName: 'Andrew',
+  lastName: 'Fung',
+  DOB: 6071912,
+  Image: 'url',
+  Age: 24,
+  roomNum: 101,
+  weight: 59,
+  height: 172,
+  emergencyContact: {
+  	firstName: 'Will',
+  	lastName: 'Fung',
+  	relationship: 'Father',
+  	phone: 59046983
+  },
+  preexistingConditions: ['high blood', 'pressure', 'asthma', 'cancer'],
+  familyHistory: [
+  	{
+  	  firstName: 'Will',
+  	  lastName: 'Fung',
+  	  relationship: 'Father',
+  	  conditions: ['high blood pressure']
+  	},
+  	{
+  	  firstName: 'Lynn',
+  	  lastName: 'Fung',
+  	  relationship: 'Mother',
+  	  conditions: ['anemia']
+  	}
+  ]
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       patients: [],
-      currentPatient: null,
+      currentPatient: testPatient,
     };
     this.handleSignout = this.handleSignout.bind(this);
   }
@@ -22,6 +57,9 @@ class App extends Component {
         />
         <PatientList
           data={this.state.patients}
+        />
+        <CurrentPatientView
+          data={this.state.currentPatient}
         />
       </div>
     );
